1f81227d9353dc5a11095e23b42364dad2becb87
Popa Andrei <andreipopa.ro@gmail.com>
2024-04-06 15:31:42 +0300

    build.data_base

--- website/models.py
+++ website/models.py
@@ -1,6 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 
 
 class Note(db.Model):
@@ -15,13 +16,11 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
-    
-# class Locatie(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     localitate = db.Column(db.String(30))
-#     strada = db.Column(db.String(30))
-#     bloc = db.Column(db.String(2))
+    user_type = db.Column(db.String(150))
+    localitate = db.Column(db.String(30))
+    strada = db.Column(db.String(30))
+    bloc = db.Column(db.String(5))
+
     
 # # Definirea tabelei de asociere (tabelul intermediar)
 # comenzi_reciclare = db.Table(
@@ -30,9 +29,9 @@ class User(db.Model, UserMixin):
 #     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
 # )
 
-# class Tip_Deseu(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     cantitate = db.Column(db.Integer)
+class Tip_Deseu(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cantitate = db.Column(db.Integer)
     
 # class Comanda(db.Model): 
 #     data = db.Column(db.DateTime(timezone=True), default=func.now())
