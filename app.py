bdfefb906069bcc9f80409c0c68c5703c6cf7638
Sabin1133 <50020278+Sabin1133@users.noreply.github.com>
2024-04-06 13:21:27 +0300

    written to app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask("ceva aplicatie")
 
 @app.route("/")
 def home():
-    return "<p>alex plm</p>"
+    return render_template("home.html")
+
+@app.route("/login")
+def loging():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signing():
+    return render_template("sign_up.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
